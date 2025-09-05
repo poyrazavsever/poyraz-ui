7c1da8b93c2347e816b96e8882b72c48640c7158
Poyraz Avsever <poyrazavsever@hotmail.com>
2025-09-05 12:26:12 +0300

    feat: done cards page site

---
+++ app/cards/page.tsx
@@ -0,0 +1,12 @@
+import CardShowcase from '@/components/card/CardShowcase'
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div>
+        <CardShowcase />
+    </div>
+  )
+}
+
+export default Cards
