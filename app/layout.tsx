1c29178b80ccb9b2697b11158f0940d37646c686
Poyraz Avsever <poyrazavsever@hotmail.com>
2025-09-05 12:02:54 +0300

    feat: done footer design and added layout with navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,12 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "UI-Kit by poyrazavsever.com",
-  description: "A UI kit for poyrazavsever.com. Created with ❤️ by Poyraz Avsever.",
+  description:
+    "A UI kit for poyrazavsever.com. Created with ❤️ by Poyraz Avsever.",
 };
+
+import Navbar from "../components/layout/navbar";
+import Footer from "../components/layout/footer";
 
 export default function RootLayout({
   children,
@@ -13,8 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
+      <body className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-1">{children}</main>
+        <Footer />
       </body>
     </html>
   );
