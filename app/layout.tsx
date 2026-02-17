1a76b96fb94b6f6e86fb9a9f2d131a226b37f23b
Poyraz Avsever <poyrazavsever@hotmail.com>
2026-02-17 15:17:45 +0300

    feat: Initialize Next.js project with core layout, homepage, Tailwind CSS, and dependency updates.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "../components/theme/theme-provider";
-import ActivityBar from "../components/layout/activitybar";
-import Sidebar from "../components/layout/sidebar";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -13,7 +10,7 @@ const nunito = Nunito({
 export const metadata: Metadata = {
   title: "UI-Kit by poyrazavsever.com",
   description:
-    "A UI kit for poyrazavsever.com. Created with ❤️ by Poyraz Avsever.",
+    "A UI kit for Poyraz Avsever. Created with ❤️ by Poyraz Avsever.",
 };
 
 export default function RootLayout({
@@ -23,16 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${nunito.className} bg-(--color-background) text-(--color-text) transition-colors duration-300`}
-      >
-        <ThemeProvider>
-          <ActivityBar />
-          <Sidebar />
-          <main className="min-h-screen pl-0 sm:pl-[calc(56px+1rem+256px)] pb-20">
-            {children}
-          </main>
-        </ThemeProvider>
+      <body>
+        {children}
       </body>
     </html>
   );
