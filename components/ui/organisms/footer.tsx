5537b9ca18c75c8836f33ef8e2053ab0cafdc02e
poyrazavsever <poyrazavsever@hotmail.com>
2026-07-11 18:14:58 +0300

    refactor(organisms): use shared utils in footer and navbar

--- components/ui/organisms/footer.tsx
+++ components/ui/organisms/footer.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/components/ui/atoms/typography";
+import { cn } from "@/lib/utils";
 
 /* ================================================================== */
 /*  FOOTER ROOT                                                        */
--- components/ui/organisms/navbar.tsx
+++ components/ui/organisms/navbar.tsx
@@ -13,7 +13,7 @@ import {
   X,
 } from "lucide-react";
 
-import { cn } from "@/components/ui/atoms/typography";
+import { cn } from "@/lib/utils";
 
 /* ================================================================== */
 /*  CONTEXT                                                            */
