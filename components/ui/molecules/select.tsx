d5efb9731badac4abb835e910cfebdcdf20c16c1
poyrazavsever <poyrazavsever@hotmail.com>
2026-07-11 18:14:57 +0300

    refactor(molecules): use shared utils in popover and select

--- components/ui/molecules/select.tsx
+++ components/ui/molecules/select.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import { Check, ChevronDown, ChevronUp } from "lucide-react";
 import * as SelectPrimitive from "@radix-ui/react-select";
 
-import { cn } from "@/components/ui/atoms/typography";
+import { cn } from "@/lib/utils";
 
 const Select = SelectPrimitive.Root;
 
